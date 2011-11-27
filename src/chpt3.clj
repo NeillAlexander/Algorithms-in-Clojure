b3537562a6c89818d6726009cc7aa5c3fd85ab42
Neill Alexander <neillalexander@gmail.com>
2011-11-27 15:13:04 +0000

    Added re-interpretation of the josephus algorithm

--- src/chpt3.clj
+++ src/chpt3.clj
@@ -27,3 +27,20 @@
          (let [sieved (find-primes 2 limit (vec (range limit)))]
            (filter (complement nil?) (drop 2 sieved)))))
 
+;; program 3.8
+;; Uses a circular list. How would one implement this in Clojure?
+;; The algorithm is so imperative, with loops. Let's write a better
+;; Clojure version.
+(defn josephus 
+  "Create a list of length n then take away every mth until there is only 1 number left."
+  [n m]
+  ;; since we are starting at one need to compensate for zero indexed sequence.
+  (let [m (dec m)]
+    (loop [circle (cycle (range 1 (inc n)))
+           dropped #{}]
+      (println (take (- n (count dropped)) circle) " " dropped)
+      (if (= (count dropped) (dec n))
+        (first circle)
+        (let [dropped (conj dropped (nth circle m))]
+          (recur (drop m (filter (complement dropped) circle)) dropped))))))
+
